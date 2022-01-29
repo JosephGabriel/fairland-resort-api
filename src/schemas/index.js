39fe75736c99a07bb76a21cbbe97facc46a1e801
JosephGabriel <josegabriel.freelancer@gmail.com>
2022-01-29 14:15:01 -0300

    Adicionados os schemas de hotel, reserva, review e de quarto

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -2,12 +2,31 @@ import { mergeTypeDefs } from "@graphql-tools/merge";
 
 import fs from "fs";
 
+const userTypeDefs = fs.readFileSync(`${__dirname}/user.graphql`).toString();
+
+const hotelTypeDefs = fs.readFileSync(`${__dirname}/hotel.graphql`).toString();
+
+const roomTypeDefs = fs.readFileSync(`${__dirname}/room.graphql`).toString();
+
 const schemaTypeDefs = fs
   .readFileSync(`${__dirname}/schema.graphql`)
   .toString();
 
-const userTypeDefs = fs.readFileSync(`${__dirname}/user.graphql`).toString();
+const bookingTypeDefs = fs
+  .readFileSync(`${__dirname}/booking.graphql`)
+  .toString();
 
-const types = [schemaTypeDefs, userTypeDefs];
+const reviewTypeDefs = fs
+  .readFileSync(`${__dirname}/review.graphql`)
+  .toString();
+
+const types = [
+  schemaTypeDefs,
+  userTypeDefs,
+  bookingTypeDefs,
+  hotelTypeDefs,
+  reviewTypeDefs,
+  roomTypeDefs,
+];
 
 export const typeDefs = mergeTypeDefs(types);
