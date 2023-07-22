6c096da45aa7a99a05ec1d990d78a34cbb802c70
JosephGabriel <josephgabriel.jgsm@gmail.com>
2023-07-22 10:51:10 -0300

    variaveis de ambiente tipadas

--- globals.d.ts
+++ globals.d.ts
@@ -1,15 +1,15 @@
 import { Server } from 'http';
 
 declare global {
-  var expressServer: Server;
-}
+  let expressServer: Server;
 
-declare namespace NodeJS {
-  interface ProcessEnv {
-    PORT: string;
-    NODE_ENV: 'production' | 'development' | 'testing';
-    SENDGRID_API_KEY: string;
-    JWT_SECRET: string;
-    JWT_EXPIRES_IN: string;
+  namespace NodeJS {
+    export interface ProcessEnv {
+      PORT: string;
+      NODE_ENV: 'production' | 'development' | 'testing';
+      SENDGRID_API_KEY: string;
+      JWT_SECRET: string;
+      JWT_EXPIRES_IN: string;
+    }
   }
 }
