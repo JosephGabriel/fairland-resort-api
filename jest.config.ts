92b93b799b0e9b4b5295fe9add29564206ad694d
JosephGabriel <josephgabriel.jgsm@gmail.com>
2023-08-19 17:46:56 -0300

    configuracao do jest atualizada

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,6 @@ import { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  watchAll: true,
   verbose: true,
   collectCoverage: true,
   globalSetup: './config/global-setup.ts',
